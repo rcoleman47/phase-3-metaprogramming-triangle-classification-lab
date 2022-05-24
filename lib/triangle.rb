16d23678f28dd705c3288ec1bf0f8ac453b5fe92
ryanadams <rcoleman.adams@gmail.com>
2022-05-23 20:15:03 -0500

    commit tree 1b2fa78c3c672bbb76a6857ef076887f5d38ea38

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  
+  def initialize(one, two, three)
+    @side_one = one
+    @side_two = two
+    @side_three = three
+  end
+
+  class TriangleError < StandardError
+    
+  end
+
+  def kind
+    sides = [@side_one, @side_two, @side_three]
+    if sides.uniq.size == 1
+      :equilateral
+    elsif sides.uniq.size == 2
+      :isosceles
+    elsif sides.uniq.size == 3
+      :scalene
+    end
+  end
+
 end
